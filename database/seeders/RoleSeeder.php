0f850354286d01fcc26c8d565227c3042fce1f53
Bakamashine <surajaaminu09@gmail.com>
2025-10-13 23:48:59 +0300

    Добавил роли и сидеры

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            "id" => 1,
+            "role_name" => "admin",
+            "abilities" => json_encode(['*']),
+        ]);
+        Role::create([
+            "id" => 2,
+            "role_name" => "user",
+            "abilities" => json_encode(['*']),
+        ]);
+    }
+}

9e75ebc3f6fe6943928dc4778e461517552773e2
Bakamashine <surajaaminu09@gmail.com>
2025-10-14 02:07:15 +0300

    Добавил мидлвар для администратора и таблицу с заказами

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Разграничивает обычного пользователя и администратора
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (\Auth::user()->role_id !== 1) {
+            // abort(403);
+            return redirect("/");
+        }
+        return $next($request);
+    }
+}

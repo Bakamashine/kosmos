41e4ab56a859d64ec87a4f94e520fc9c98d53a43
Bakamashine <surajaaminu09@gmail.com>
2025-11-12 02:14:43 +0300

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{-- <title>@php echo config('app.name') @endphp</title> --}}
+    <link rel="shortcut icon" href="/img/rocket.png" type="image/png">
     @viteReactRefresh
     @vite(['resources/js/app.tsx', 'resources/css/app.css', 'resources/css/main.css', 'resources/js/bootstrap.js'])
     @inertiaHead

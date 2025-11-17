198fba4f91fc09abc17dd96e473c6e946fcbdae3
Bakamashine <surajaaminu09@gmail.com>
2025-11-17 18:00:20 +0300

    Create new command on artisan

---
+++ app/Console/Commands/createroles.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Database\Seeders\RoleSeeder;
+use Illuminate\Console\Command;
+
+class createroles extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:createroles';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        (new RoleSeeder())->run();
+    }
+}
---
+++ app/Console/Commands/createsuperuser.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class createsuperuser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:createsuperuser';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Создаёт стартового администратора';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        User::create([
+            "role_id" => 1,
+            "name" => "admin",
+            "email" => "admin@admin.ru",
+            "password" => bcrypt("admin")
+        ]);
+    }
+}

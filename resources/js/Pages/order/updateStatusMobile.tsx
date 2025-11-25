334c263f93782ec65adb64c95cf10bcf36c1eb5d
Bakamashine <surajaaminu09@gmail.com>
2025-11-25 14:48:42 +0300

    Add tag with text "not found"

--- resources/js/Pages/order/updateStatusMobile.tsx
+++ resources/js/Pages/order/updateStatusMobile.tsx
@@ -5,6 +5,7 @@ import { route } from "ziggy-js";
 import { Order, OrderPag } from "../../interface";
 import Layout from "../Layout";
 import Paginate from "../../components/ui/Paginate";
+import NotFoundRecords from "../../components/ui/NotFoundRecords";
 export default function UpdateStatusMobile() {
     const { props } = usePage();
     const orders = props.order as OrderPag;
@@ -21,47 +22,62 @@ export default function UpdateStatusMobile() {
     const title = "Обновление статуса";
     return (
         <Layout title={title} title_h1>
-            <div className="news">
-                {orders.data.map((item, index) => (
-                    <Card key={index} style={{ width: "18rem" }}>
-                        <Card.Body>
-                            <Card.Title>{item.flying_title}</Card.Title>
-                            <Card.Text>{item.status}</Card.Text>
-                            <Card.Text>{item.user_name}</Card.Text>
+            <div>
+                {orders.data && orders.data.length > 0 ? (
+                    <div className="news">
+                        {orders.data.map((item, index) => (
+                            <Card key={index} style={{ width: "18rem" }}>
+                                <Card.Body>
+                                    <Card.Title>{item.flying_title}</Card.Title>
+                                    <Card.Text>{item.status}</Card.Text>
+                                    <Card.Text>{item.user_name}</Card.Text>
 
-                            <Card.Footer>
-                                <Form onSubmit={(e) => submit(e, item.id)}>
-                                    <Form.Select
-                                        aria-label="Default select example"
-                                        onChange={(data) =>
-                                            setData("status", data.target.value)
-                                        }
-                                        defaultValue={item.status}
-                                        // value={data.status}
-                                    >
-                                        {orders_status.map((item, index) => (
-                                            <option key={index}>{item}</option>
-                                        ))}
-                                    </Form.Select>
-                                    <div className="mt-2 d-flex justify-content-center">
-                                        <Button
-                                            type={"submit"}
-                                            className="btn btn-dark"
+                                    <Card.Footer>
+                                        <Form
+                                            onSubmit={(e) => submit(e, item.id)}
                                         >
-                                            Смена статуса
-                                        </Button>
-                                    </div>
-                                </Form>
-                            </Card.Footer>
-                        </Card.Body>
-                    </Card>
-                ))}
+                                            <Form.Select
+                                                aria-label="Default select example"
+                                                onChange={(data) =>
+                                                    setData(
+                                                        "status",
+                                                        data.target.value
+                                                    )
+                                                }
+                                                defaultValue={item.status}
+                                                // value={data.status}
+                                            >
+                                                {orders_status.map(
+                                                    (item, index) => (
+                                                        <option key={index}>
+                                                            {item}
+                                                        </option>
+                                                    )
+                                                )}
+                                            </Form.Select>
+                                            <div className="mt-2 d-flex justify-content-center">
+                                                <Button
+                                                    type={"submit"}
+                                                    className="btn btn-dark"
+                                                >
+                                                    Смена статуса
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                    </Card.Footer>
+                                </Card.Body>
+                            </Card>
+                        ))}
+                        {orders.total > 5 && (
+                            <div className="d-flex justify-content-center">
+                                <Paginate item={orders} />
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <NotFoundRecords text="Заявок нет" />
+                )}
             </div>
-            {orders.total > 5 && (
-                <div className="d-flex justify-content-center">
-                    <Paginate item={orders} />
-                </div>
-            )}
         </Layout>
     );
 }
